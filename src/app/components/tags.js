e2d0972f4437e9f6aad5ea322d5ed91ceaad8393
ArNisha Henderson <arnisha34@yahoo.com>
2025-05-08 00:12:34 -0400

    updated tags array to remove dupes and fixed styling

--- src/app/components/tags.js
+++ src/app/components/tags.js
@@ -1,17 +1,18 @@
-import { useContext } from "react"
-import { Context } from "../context/context"
+import { useSelector } from "react-redux"
 import { TagIcon } from "@heroicons/react/24/outline"
 
 
 export const Tags = () => {
 
-  const ctx = useContext(Context)
+  const getNotes = useSelector(state => state.notes.notes)
+
+  const allTags = [...new Set(getNotes.flatMap(note => note.tags || []))]
 
   return(
     <section id="tags" className="px-3">
       <h5 className="py-3 text-gray-500">Tags</h5>
       <div className="flex flex-col gap-2">
-        {(ctx.tags ?? []).map((tag, id) => <p key={id} className="cursor-pointer flex gap-2 p-2 rounded-lg hover:bg-neutral-100"><TagIcon className="w-4 stroke-2"/>{tag}</p>)}
+        {[...new Set(allTags?.flat())].map((tag, id) => <p key={id} className="cursor-pointer flex gap-2 p-2 rounded-lg hover:bg-neutral-100"><TagIcon className="w-4 stroke-2"/>{tag}</p>)}
       </div>
     </section>
   )
